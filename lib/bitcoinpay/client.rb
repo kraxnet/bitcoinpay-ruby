16357726047e7c9ddea10572f67e2ce12755eda1
Jiri Kubicek <jiri.kubicek@kraxnet.cz>
2016-07-06 01:30:53 +0200

    All missing api calls

--- lib/bitcoinpay/client.rb
+++ lib/bitcoinpay/client.rb
@@ -19,14 +19,37 @@ module Bitcoinpay
 
     def create_payment_request(values)
       options={body: values.to_json, headers: headers}
-      output = self.class.post("/payment/btc", options)
+      self.class.post("/payment/btc", options)
+    end
+
+    def payment(payment_id)
+      self.class.get("/payment/btc/#{payment_id}", {headers: headers})
+    end
+
+    def rates
+      self.class.get("/rates/btc", {headers: headers})
     end
 
     # status=status&date_start=date_start&date_end=date_end&sort=sort&limit=limit&offset=offset
     def transaction_history(query = {})
       options = { query: query}
       options.merge!({ headers: headers })
-      output = self.class.get("/transaction-history/", options)
+      self.class.get("/transaction-history/", options)
+    end
+
+    def transaction(payment_id)
+      self.class.get("/transaction-history/#{payment_id}", { headers: headers })
+    end
+
+    #status settlement_account settlement_currency sort limit offset date_start date_end
+    def settlement_history(query = {})
+      options = { query: query}
+      options.merge!({ headers: headers })
+      self.class.get("/settlement-history/", options)
+    end
+
+    def settlement(settlement_id)
+      self.class.get("/settlement-history/#{settlement_id}", { headers: headers })
     end
   end
 end
